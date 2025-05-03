aa7b13e1f0b297ce4c7b46505b068aab992ea188
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-05-03 19:40:13 +0300

    feat: implement toggle functionality for navbar menu

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const ToggleMenu = (function () {
+  const navbarToggleButton = document.getElementById("navbar-hamburger");
+  const navbarCloseButton = document.getElementById("navbar-close");
+  const navbarList = document.getElementById("navbar-list");
+
+  navbarToggleButton.addEventListener("click", navbarToggleHandler);
+  navbarCloseButton.addEventListener("click", navbarToggleHandler);
+
+  function navbarToggleHandler(event) {
+    navbarToggleButton.classList.toggle("is-hidden");
+    navbarCloseButton.classList.toggle("is-hidden");
+
+    navbarList.classList.toggle("js-show");
+  }
+})();
