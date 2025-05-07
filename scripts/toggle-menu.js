aee2ee1dd7a247fedb9ff871abde064bf9a9dc79
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-05-07 21:20:16 +0300

    feat: add JSDoc comments for init & navbarToggleHandler functions

--- scripts/toggle-menu.js
+++ scripts/toggle-menu.js
@@ -1,6 +1,13 @@
 "use strict";
 
 const ToggleMenu = (function () {
+  /**
+   * Initializes the responsive navigation menu.
+   *
+   * @function init
+   * @param {void}
+   * @returns {void}
+   */
   function init() {
     const navbarToggleButton = document.getElementById("navbar-hamburger");
     const navbarCloseButton = document.getElementById("navbar-close");
@@ -9,6 +16,14 @@ const ToggleMenu = (function () {
     navbarToggleButton.addEventListener("click", navbarToggleHandler);
     navbarCloseButton.addEventListener("click", navbarToggleHandler);
 
+    /**
+     * Toggles the visibility of the responsive navigation menu and
+     * its toggle/close buttons.
+     *
+     * @function navbarToggleHandler
+     * @param {Event} event - The event that triggered this function.
+     * @returns {void}
+     */
     function navbarToggleHandler(event) {
       navbarToggleButton.classList.toggle("is-hidden");
       navbarCloseButton.classList.toggle("is-hidden");
